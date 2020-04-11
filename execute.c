5d085ae555e6677c93cfee24df05c8a7b47c06ec
David Orozco <dvdizcky@gmail.com>
2020-04-11 10:39:22 -0500

    Betty style applied

--- execute.c
+++ execute.c
@@ -4,7 +4,7 @@
  * execute - Execute a command in other process
  *
  * @command: Command to execute
- * @argument: Arguments of the @command
+ * @arguments: Arguments of the @command
  **/
 void execute(char *command, char **arguments)
 {

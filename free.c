b33204fecd9e05664f5f75de6f0f4e87244a4656
David Orozco <dvdizcky@gmail.com>
2020-04-16 09:40:08 -0500

    Memory management: Free controller added

---
+++ free.c
@@ -0,0 +1,32 @@
+#include "memory.h"
+
+/**
+ * free_memory_p - Free a pointer
+ *
+ * @ptr: Pointer to free
+ **/
+void free_memory_p(void *ptr)
+{
+	if (ptr != NULL)
+	{
+		free(ptr);
+		ptr = NULL;
+	}
+
+}
+
+/**
+ * free_memory_pp - Free a double pointer
+ *
+ * @ptr: Double pointer to free
+ **/
+void free_memory_pp(void **ptr)
+{
+	void **tmp;
+
+	for (tmp = ptr; *tmp != NULL; tmp++)
+		free_memory_p(*tmp);
+
+	free_memory_p(ptr);
+}
+
--- memory.h
+++ memory.h
@@ -7,5 +7,8 @@
 /* memory.c */
 void *_realloc(void *ptr, size_t old_size, size_t new_size);
 
+/* free.c */
+void free_memory_p(void *ptr);
+void free_memory_pp(void **ptr);
 
 #endif /* MEMORY_H */

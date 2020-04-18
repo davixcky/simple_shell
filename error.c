b519b15a080c365c3da585c25a8ba36938d5d71a
David Orozco <dvdizcky@gmail.com>
2020-04-18 16:44:20 -0500

    Support to format <normal_message: extra_info>

--- error.c
+++ error.c
@@ -60,3 +60,34 @@ void error(general_t *info)
 	free(aux);
 }
 
+void error_extra(general_t *info, char *extra)
+{
+	char *message, *number, *aux, *aux2;
+	int size_number, size_message, size_extra;
+
+	number = NULL;
+	message = message_selector(*info);
+	number = to_string(info->n_commands);
+
+	size_number = _strlen(number);
+	size_message = _strlen(info->argv[0]);
+	size_extra = _strlen(extra);
+
+	aux = malloc(size_message + size_number + 3);
+	aux = _strcpy(aux, info->argv[0]);
+	aux = _strcat(aux, ": ");
+	aux = _strcat(aux, number);
+
+	aux2 = malloc(_strlen(message) + size_extra + 3);
+	aux2 = _strcpy(aux2, message);
+	aux2 = _strcat(aux2, ": ");
+	aux2 = _strcat(aux2, extra);
+
+	message = join_words(aux, info->command, aux2, ": ");
+	print_err(message);
+
+	free(message);
+	free(number);
+	free(aux);
+	free(aux2);
+}
--- error.h
+++ error.h
@@ -7,5 +7,6 @@
 /* error.c */
 char *message_selector(general_t info);
 void error(general_t *info);
+void error_extra(general_t *info, char *extra);
 
 #endif /* ERROR_H */
